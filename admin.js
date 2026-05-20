643c693b3668d8b403a047ff528e164cc809b9cb
WillElias374 <146851033+WillElias374@users.noreply.github.com>
2026-05-20 09:52:43 -0400

    About me button

--- admin.js
+++ admin.js
@@ -60,6 +60,12 @@ export const showItems = async function(firingType) {
     })
     return
   }
+  if (firingType == "aboutMe") {
+    const column = document.getElementById("infoColumn")
+    const name = document.creatElement("div")
+    name.innerHTML = "Will"
+    //const img = document.creatElement("div")
+  }
   const column = document.getElementById("infoColumn")
   const snapshot = await getDocs(query(collection(db, firingType), orderBy("createdAt", "asc")))
   for(const item of snapshot.docs) {
