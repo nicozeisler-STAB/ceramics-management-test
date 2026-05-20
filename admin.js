c0ad5e30bd308dcc3932152541f6b64e594893a9
WillElias374 <146851033+WillElias374@users.noreply.github.com>
2026-05-20 09:27:08 -0400

    Minor changes

--- admin.js
+++ admin.js
@@ -60,13 +60,6 @@ export const showItems = async function(firingType) {
     })
     return
   }
-
-  if (firingType == "aboutMe") {
-    const column = document.getElementById("infoColumn")
-    const name = document.CreateElement("div")
-    name.innerHTML = "Will"
-  }
-  
   const column = document.getElementById("infoColumn")
   const snapshot = await getDocs(query(collection(db, firingType), orderBy("createdAt", "asc")))
   for(const item of snapshot.docs) {
